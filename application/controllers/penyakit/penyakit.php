0f312246118f2b554741fb43eb36730fabb10cad
Farhanzidanapps <111355899+zidanfarhan23333333@users.noreply.github.com>
2023-12-18 23:52:00 +0700

    Update penyakit.php

--- application/controllers/penyakit/penyakit.php
+++ application/controllers/penyakit/penyakit.php
@@ -1,105 +1,101 @@
-<?php 
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    class Penyakit extends CI_Controller {
-        function __construct ()
-        {
-            parent::__construct();
-        }
-        function index() {
-            $this->load->view('partials/sidebar');
-            $this->load->view('penyakit/penyakit_index');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Penyakit extends CI_Controller {
+    function __construct ()
+    {
+        parent::__construct();
+    }
+
+    function index() {
+        $data['penyakit'] = $this->M_data->get_data('penyakit')->result();
+        $this->load->view('partials/header');
+        $this->load->view('penyakit/penyakit', $data);
+        $this->load->view('partials/footer');
+    }
+
+    function penyakit_tambah() {
+        $this->load->view('partials/header');
+        $this->load->view('penyakit/penyakit_create');
+        $this->load->view('partials/footer');
+    }
+
+    function penyakit_tambah_aksi() {
+        $this->form_validation->set_rules('code_penyakit', 'Kode Penyakit', 'required|is_unique[penyakit.code_penyakit]',
+            array('is_unique' => 'Kode Penyakit ini sudah ada. Tolong Inputkan kode penyakit yang berbeda.')
+        );
+        $this->form_validation->set_rules('nama_penyakit', 'Nama Penyakit', 'required');
+        $this->form_validation->set_rules('definisi', 'Definisi', 'required');
+        $this->form_validation->set_rules('pengobatan', 'Pengobatan', 'required');
+    
+        if ($this->form_validation->run() != false) {
+            $code_penyakit = $this->input->post('code_penyakit');
+            $nama_penyakit = $this->input->post('nama_penyakit');
+            $definisi = $this->input->post('definisi');
+            $pengobatan = $this->input->post('pengobatan');
+    
+            $data = array(
+                'code_penyakit' => $code_penyakit,
+                'nama_penyakit' => $nama_penyakit,
+                'definisi' => $definisi,
+                'pengobatan' => $pengobatan
+            );
+    
+            $this->M_data->insert_data($data, 'penyakit');
+            redirect(base_url() . 'penyakit/penyakit');
+        } else {
+ 
+            $this->load->view('partials/header');
+            $this->load->view('penyakit/penyakit_create');
             $this->load->view('partials/footer');
         }
-
-        function logout() {
-            $this->session->sess_destroy();
-            redirect(base_url().'login/?alert=logout');
-        }
-        function ganti_password() {
-            $this->load->view('admin/v_header');
-            $this->load->view('admin/v_ganti_password');
-            $this->load->view('admin/v_footer');
-        }
-        function ganti_password_aksi() {
-            $baru = $this->input->post('password_baru');
-            $ulang = $this->input->post('password_ulang');
-            $this->form_validation->set_rules('password_baru', 'Password Baru', 'required|matches[password_ulang]');
-            $this->form_validation->set_rules('password_ulang', 'Ulangi Password', 'required');
-            if($this->form_validation->run() != false) 
-            {
-                $id= $this->session->userdata('id');
-                $where = array('id' => $id);
-                $data = array('password' => md5($baru));
-                $this->M_data->update_data($where,$data,'admin');
-                redirect(base_url().'admin/ganti_password/?alert=sukses');
-            } else
-            {
-                $this->load->view('admin/v_header');
-                $this->load->view('admin/v_ganti_password');
-                $this->load->view('admin/v_footer');
-            }
-        }
-        function petugas() {
-            $data['petugas'] = $this->M_data->get_data('m_biodata_pegawai')->result();
-            $this->load->view('admin/v_header');
-            $this->load->view('admin/v_petugas', $data);
-            $this->load->view('admin/v_footer');
-        }
-        function petugas_tambah() {
-            $this->load->view('admin/v_header');
-            $this->load->view('admin/v_petugas_tambah');
-            $this->load->view('admin/v_footer');
-        }
-        function petugas_tambah_aksi() {
-            $nik = $this->input->post('nik');
-            $nama = $this->input->post('nama');
-            $username = $this->input->post('username');
-            $password = $this->input->post('password');
-            $data = array (
-                'NIK' => $nik,
-                'M_biodata_pegawai_nama' => $nama,
-                'username' => $username,
-                'password' => md5($password)
+    }
+    
+    function penyakit_edit($id_penyakit) {
+        $where = array('id_penyakit' => $id_penyakit);
+        $data['penyakit'] = $this->M_data->edit_data($where, 'penyakit')->result();
+        $this->load->view('partials/header');
+        $this->load->view('penyakit/penyakit_edit', $data);
+        $this->load->view('partials/footer');
+    }
+    
+    function penyakit_update() {
+        $id_penyakit = $this->input->post('id_penyakit');
+        $code_penyakit = $this->input->post('code_penyakit');
+        $nama_penyakit = $this->input->post('nama_penyakit');
+        $definisi = $this->input->post('definisi');
+        
+        $this->form_validation->set_rules('code_penyakit', 'Kode Penyakit', 'required|is_unique[penyakit.code_penyakit]',
+            array('is_unique' => 'Kode Penyakit sudah ada. Inputkan kode penyakit yang berbeda.')
+        );
+        $this->form_validation->set_rules('nama_penyakit', 'Nama Penyakit', 'required');
+        
+        if ($this->form_validation->run() != false) {
+            $where = array('id_penyakit' => $id_penyakit);
+            $data = array(
+                'code_penyakit' => $code_penyakit,
+                'nama_penyakit' => $nama_penyakit,
+                'definisi' => $definisi,
             );
-            $this->M_data->insert_data($data,'m_biodata_pegawai');
-            redirect(base_url().'admin/petugas');
-        }
-        function petugas_edit($id) {
-            $where = array ('id' => $id);
-            $data['petugas'] = $this->M_data->edit_data($where, 'm_biodata_pegawai')->result();
-            $this->load->view('admin/v_header');
-            $this->load->view('admin/v_petugas_edit', $data);
-            $this->load->view('admin/v_footer');
-        }
-        function petugas_update() {
-            $id = $this->input->post('id');
-            $nik = $this->input->post('nik');
-            $nama = $this->input->post('nama');
-            $username = $this->input->post('username');
-            $password = $this->input->post('password');
-            $where = array('id' => $id);
-            if($password=="") {
-                $data = array(
-                    'NIK' =>$nik,
-                    'M_biodata_pegawai_nama'=> $nama,
-                    'username' => $username, 
-                );
-                $this->M_data->update_data($where, $data, 'm_biodata_pegawai');
-            } else {
-                $data = array(
-                    'NIK' =>$nik,
-                    'M_biodata_pegawai_nama'=> $nama,
-                    'username' => $username, 
-                    'password' => md5($password)
-                );
-                $this->M_data->update_data($where, $data, 'm_biodata_pegawai');   
-            }
-            redirect(base_url().'admin/petugas');
-        } 
-        function petugas_hapus($id) {
-            $where = array('id' => $id);
-            $this->M_data->delete_data($where,'m_biodata_pegawai');
-            redirect(base_url().'admin/petugas');
+            $this->M_data->update_data($where, $data, 'penyakit');
+            $this->session->set_flashdata('success_message', 'Penyakit berhasil diedit.');
+            redirect(base_url() . 'penyakit');
+        } else {
+            $where = array('id_penyakit' => $id_penyakit);
+            $data['penyakit'] = $this->M_data->edit_data($where, 'penyakit')->result();
+        
+            $this->load->view('partials/header');
+            $this->load->view('penyakit/penyakit_edit', $data);
+            $this->load->view('partials/footer');
         }
     }
+    
+    
+
+    function penyakit_hapus($id_penyakit) {
+        $where = array('id_penyakit' => $id_penyakit);
+        $this->M_data->delete_data($where, 'penyakit');
+        redirect(base_url().'penyakit/penyakit');
+    }
+}
 ?>
