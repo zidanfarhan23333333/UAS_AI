d4ecd910ff3ba1d4f5477a8b35ebdb6da113cc72
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-12-26 21:50:21 +0700

    selesai dengan logic naive bayes yang sangat angel

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -36,8 +36,10 @@
                 $tahap1 = 0;
                 $tahap2 = 0; 
                 $tahap3 = 0; 
-
                 $tahap4 = 0; 
+                $tahap5 = 0; 
+                $tahap6 = 0; 
+                $tahap7 = 0; 
 
                 // Tahap 1 
                 foreach ($idGejala as $idGejalaInptanUser) {
@@ -45,20 +47,22 @@
                         if ($probabilitasPakar->id_gejala == $idGejalaInptanUser) {
                             $tahap1 = (float)$probabilitasPakar->bobot;
                             $tahap2 += $tahap1;
+                            // echo "Hasil untuk id_penyakit $idPenyakit:\n";
+                            // echo "tahap ke-1: $tahap1\n";
                             break;
                         }
                     }
                 }
                 
-                // Tahap 2 dan Tahap 3
+                // Tahap 2, Tahap 3 & tahap 4
                 foreach ($idGejala as $idGejalaInptanUser) {
                     foreach ($ruleProbabilitas as $probabilitasPakar) {
                         if ($probabilitasPakar->id_gejala == $idGejalaInptanUser) {
                             $tahap1 = (float)$probabilitasPakar->bobot;
                             $tahap3 = $tahap1 / $tahap2;
-
+                            
                             $tahap4 += $tahap1 * $tahap3;
-
+                            
                             // echo "Hasil untuk id_penyakit $idPenyakit:\n";
                             // echo "tahap ke-1: $tahap1\n";
                             // echo "tahap ke-2: $tahap2\n";
@@ -68,13 +72,47 @@
                         }
                     }
                 }
+
+                // Tahap 5 
+                foreach ($idGejala as $idGejalaInptanUser) {
+                    foreach ($ruleProbabilitas as $probabilitasPakar) {
+                        if ($probabilitasPakar->id_gejala == $idGejalaInptanUser) {
+                            $tahap1 = (float)$probabilitasPakar->bobot;
+                            $tahap3 = $tahap1 / $tahap2;
+
+                            $tahap5 = $tahap1 * $tahap3 / $tahap4;
+
+                            $tahap6 += $tahap1 * $tahap5;
+
+                            // echo "Hasil untuk id_penyakit $idPenyakit:\n";
+                            // echo "tahap ke-1: $tahap1\n";
+                            // echo "tahap ke-3: $tahap3\n";
+                            // echo "tahap ke-5: $tahap5\n";
+                            // echo "tahap ke-6: $tahap6\n";
+                            
+                            break;
+                        }
+                    }
+                }
+
+                // Tahap 7
+                $tahap7 = $tahap6 * 100;
+
+                if ($tahap7 > $maxResult) {
+                    $maxResult = $tahap7;
+                    $namaPenyakitPersentaseTerbesar = $this->getNamaPenyakitById($idPenyakit);
+                }
                 
-                echo "Hasil untuk id_penyakit $idPenyakit:\n";
+                // echo "Hasil untuk id_penyakit $idPenyakit:\n";
                 // echo "tahap ke-1 : $tahap1\n";
                 // echo "tahap ke-2 : $tahap2\n";
                 // echo "tahap ke-3: $tahap3\n";
-                echo "tahap ke-4: $tahap4\n";
+                // echo "tahap ke-4: $tahap4\n";
+                // echo "tahap ke-5: $tahap5\n";
+                // echo "tahap ke-6: $tahap6\n";
+                // echo "tahap ke-7: $tahap7 %";
             }
+            echo "Penyakit yang dialami pasien adalah: $namaPenyakitPersentaseTerbesar , dengan tingkat keyakinan: $maxResult %";
         
             $this->load->view('user/diagnosa/diagnosa_result');
         }
